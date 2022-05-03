ba9914495c505efcec156cb3b2bb910573d1d292
Ashlynn <ashlynn.steeves@icloud.com>
2022-05-03 19:28:33 -0400

    include fcn call

--- May3_Exercise.py
+++ May3_Exercise.py
@@ -1,5 +1,5 @@
 def my_function(input1, input2):
-	
+	# type check 
 	multiply = input1*input2
 	print('Multiplication: ', multiply)
 	divide = input1/input2
@@ -8,3 +8,6 @@ def my_function(input1, input2):
 	print('Addition: ', add)
 	subtract = input1 - input2 
 	print('Subtraction: ', subtract)
+
+
+my_function(3,4)

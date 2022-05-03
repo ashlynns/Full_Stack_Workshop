6e619c2ffb2b8f89ad4f1d9273834a4dc693ec29
Ashlynn <ashlynn.steeves@icloud.com>
2022-05-03 19:24:58 -0400

    May 3 docs

---
+++ May3_Exercise.py
@@ -0,0 +1,10 @@
+def my_function(input1, input2):
+	
+	multiply = input1*input2
+	print('Multiplication: ', multiply)
+	divide = input1/input2
+	print('Division: ', divide)
+	add = input1+input2 
+	print('Addition: ', add)
+	subtract = input1 - input2 
+	print('Subtraction: ', subtract)
